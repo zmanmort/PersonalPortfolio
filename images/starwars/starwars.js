cc5604af05211cc55f63aa1a2d65032ceccad855
Zach Mortensen <10921971@my.uvu.edu>
2021-03-25 19:12:29 -0600

    added data files and for loop to show list

--- images/starwars/starwars.js
+++ images/starwars/starwars.js
@@ -1,3 +1,18 @@
-const message = 'hello world'
+import { films } from '.../data/films.js'
 
-console.log(message)
+/* let itemOne = document.querySelector('#item1')
+let itemTwo = document.querySelector('#item2')
+
+itemOne.textContent = films[2].title
+itemTwo.textContent = films[1].title */
+
+//console.log(films[0].title)
+
+let titleist = document.querySelector('.titlelist')
+
+for (var i = 0; i < films.length; i++) {
+    let title = films[i].title
+    let newItem = document.createElement('li')
+    newItem.textContent = Title
+    titlelist.appendCild(newItem)
+}
